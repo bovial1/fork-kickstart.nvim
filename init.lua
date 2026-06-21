4b1f4319a58d2df53cc6cd7dbb7b6ba10300a53f
Bo Vial <bvial@macquariecloudservices.com>
2026-06-21 15:56:51 +1000

    further init

--- init.lua
+++ init.lua
@@ -230,6 +230,32 @@ do
 
   vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+  vim.g.diagnostics_visible = true
+  function _G.toggle_diagnostics()
+    if vim.g.diagnostics_visible then
+      vim.g.diagnostics_visible = false
+      vim.diagnostic.enable(false)
+    else
+      vim.g.diagnostics_visible = true
+      vim.diagnostic.enable()
+    end
+  end
+
+  vim.g.diagnostics_virtual_text_visible = true
+  function _G.toggle_diagnostics_virtual_text()
+    if vim.g.diagnostics_virtual_text_visible then
+      vim.g.diagnostics_virtual_text_visible = false
+      vim.diagnostic.config { virtual_text = false }
+    else
+      vim.diagnostic.config { virtual_text = true }
+      vim.g.diagnostics_virtual_text_visible = true
+    end
+  end
+
+  vim.keymap.set({ 'n' }, '<leader>i', toggle_diagnostics, { desc = 'Toggle d[i]agnostics' })
+  vim.keymap.set({ 'n' }, '<leader>I', toggle_diagnostics_virtual_text, { desc = 'Toggle d[I]agnostics virtual text' })
+
+
   -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
   -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
   -- is not what someone will guess without a bit more experience.
